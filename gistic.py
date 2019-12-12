4380608ba230681299707a9bc26ea825351e7146
Spenca <vatrtwaa@mskcc.org>
2019-12-12 15:59:32 -0500

    Add beginnings of transform function. Currently calculates first and last terms for numerator and denominator of weighted average calculations

--- gistic.py
+++ gistic.py
@@ -80,11 +80,60 @@ def extract(gtf, hgnc, igv_segs, titan_segs):
 
 def transform():
 	# perform weighted averge calculations and required transformations
-	pass
+	seg_dict, log_dict = {}, {}
+	extracted_file = open('extract.txt','r')
+	next(extracted_file)
+	file_reader = csv.reader(extracted_file, delimiter='\t')
+	for line in file_reader:
+		if line[7]:
+			# if seg_start <= gene_start and seg_end >= gene_end
+			if int(line[1]) <= int(line[10]) and int(line[2]) >= int(line[11]):
+				# map median_logR to ensembl_id
+				log_dict[line[7]] = line[3]
+			else:
+				if line[7] not in seg_dict:
+					seg_dict[line[7]] = [[], [], [], 0, 0]
+				
+				seg_dict[line[7]][0].append(int(line[1]))
+				seg_dict[line[7]][1].append(int(line[2]))
+				seg_dict[line[7]][2].append(float(line[3]))
+				seg_dict[line[7]][3] = int(line[10])
+				seg_dict[line[7]][4] = int(line[11])
+	
+	for ensembl_id in seg_dict:
+		seg_starts = sorted([start for start in seg_dict[ensembl_id][0]])
+		seg_ends = sorted([end for end in seg_dict[ensembl_id][1]])
+		segs_to_remove = []
+		for seg_start, seg_end in zip(seg_starts, seg_ends):
+			if seg_start == seg_end:
+				segs_to_remove.append(seg_start)
+		
+		for seg in segs_to_remove:
+			seg_starts.remove(seg), seg_ends.remove(seg)
+		
+		if len(seg_starts) <= 1:
+			if len(seg_starts) == 1:
+				log_dict[ensembl_id] = seg_dict[ensembl_id][2][0]
+			
+			continue
+
+		seg_logs = [log for log in seg_dict[ensembl_id][2]]
+		gene_start = seg_dict[ensembl_id][3]
+		gene_end = seg_dict[ensembl_id][4]
+		denominator_start = (min(seg_ends) - gene_start) / (min(seg_ends) - min(seg_starts))
+		denominator_end = ((max(seg_ends) - max(seg_starts)) - (max(seg_ends) - gene_end)) / (max(seg_ends) - max(seg_starts))
+		numerator_start = denominator_start * seg_logs[seg_starts.index(min(seg_starts))] 
+		numerator_end = denominator_end * seg_logs[seg_starts.index(max(seg_starts))]
+		# TODO: drop mins and maxes, calculate rest of segs
+		numerator_rest = 0
+		denominator_rest = 0				
+
+	print(seg_dict)
+	return log_dict
 
 
-def load():
-	# split generated file into the four output 
+def load(log_dict):
+	# split generated file into the four outputs
 	pass
 
 
