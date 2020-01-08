6acfda9918fd0a48233786730ac58b17f2808d06
Spenca <vatrtwaa@mskcc.org>
2020-01-08 16:32:09 -0500

    Add test code for merge script

--- test_merge.py
+++ test_merge.py
@@ -5,10 +5,15 @@ from merge_gene_outputs import merge_gene_outputs
 
 
 @click.command()
-@click.option('--input_dir', default='')
-@click.option('--output_dir', default='')
+@click.option('--input_dir', default='test/merge_gene_outputs/test_input/')
+@click.option('--output_dir', default='test/merge_gene_outputs/test_output/')
 def main(input_dir, output_dir):
     merge_gene_outputs(input_dir, output_dir)
+
+    if filecmp.cmp('test/merge_gene_outputs/output_baseline/merged.txt', output_dir + 'merged.txt'):
+        print('Merged file matches baseline.')
+    else:
+        print('Merged file does not matche baseline!')
 
 
 if __name__ == '__main__':
