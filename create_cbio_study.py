51c6f31a65e2b44a98df1b7dc48b7bdb4510e186
Spenca <vatrtwaa@mskcc.org>
2020-03-17 17:13:16 -0400

    Add read_gene_data

--- create_cbio_study.py
+++ create_cbio_study.py
@@ -216,6 +216,7 @@ def main(input_yaml, path_to_output_study, temp_dir):
 
                 elif doc['datatype'] == 'SCWGS':
                     cnv = hmmcopy.read_copy_data(doc['hmmcopy_csv'], filter_normal=doc['filter_normal'])
+                    genes = hmmcopy.read_gene_data(gtf_file)
 
                 else:
                     raise ValueError(f'unrecognized data type {doc["datatype"]}')
--- hmmcopy.py
+++ hmmcopy.py
@@ -89,7 +89,7 @@ def calculate_gene_copy(cnv, genes):
             # Subset overlapping genes
             overlapping_genes = chr_genes[~((chr_genes['gene_end'] < row['start']) | (chr_genes['gene_start'] > row['end']))]
 
-            # Add cnv columns
+            # TODO: add cnv columns
             overlapping_genes
 
             data.append(overlapping_genes)
@@ -99,12 +99,13 @@ def calculate_gene_copy(cnv, genes):
     return data
 
 
+# TODO: finish test skeleton
 def test_calculate_gene_copy():
     # Create some cnv data
     cnv = pd.DataFrame({})
 
     # Create some gene data
-    gene = pd.DataFrame({})
+    genes = pd.DataFrame({})
 
     # Create known overlap
     overlapping = pd.DataFrame({})
@@ -149,3 +150,20 @@ def read_copy_data(bins_filename, filter_normal=False):
     )
 
     return data
+
+
+def read_gene_data(gtf):
+    data = pd.read_csv(gtf, delimiter='\t', names=['chr', 'gene_start', 'gene_end', 'gene_id'], usecols=[0,3,4,8])
+    data['gene_id'] = data['gene_id'].str.extract('(ENSG\d+)')
+    data = data.groupby('gene_id').agg({'gene_start':'min', 'gene_end':'max'}).reset_index()
+
+    return data
+
+
+# Testing
+def main():
+    read_gene_data('Homo_sapiens.GRCh37.73.gtf')
+
+
+if __name__ == '__main__':
+    main()
