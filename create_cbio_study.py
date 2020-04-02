37db4e525d26c456c8637cef51b4ba177cd33cb6
Spenca <vatrtwaa@mskcc.org>
2020-04-01 23:34:17 -0400

    Minor: debug

--- create_cbio_study.py
+++ create_cbio_study.py
@@ -224,8 +224,8 @@ def main(input_yaml, path_to_output_study, temp_dir):
                     overlapping = hmmcopy.calculate_gene_copy(cnv, genes)
                     hmmcopy.convert_to_transform_format(overlapping, hgnc_file, temp_dir)
 
-                    extracted_file = open(temp_dir + 'hmmcopy_extract', 'r')
-                    gene_dict, seg_dict = transform(extracted_file, show_missing_hugo=False, show_missing_entrez=False, show_missing_both=False)
+                    hmmcopy_extract = open(temp_dir + 'hmmcopy_extract', 'r')
+                    gene_dict, seg_dict = transform(hmmcopy_extract, show_missing_hugo=False, show_missing_entrez=False, show_missing_both=False)
                     load(gene_dict, seg_dict, sample, temp_dir, output_gistic_gene=True, output_integer_gene=False, output_log_seg=True, output_integer_seg=False)
 
                 else:
