bba8e7ac17adfc35b9df2cb5f3345808e7e9728a
Spenca <vatrtwaa@mskcc.org>
2020-06-16 20:32:01 -0400

    Oops

---
+++ utils.py
@@ -0,0 +1,100 @@
+import gzip
+import os
+
+import errno
+import multiprocessing
+
+from subprocess import Popen, PIPE
+
+class OpenFile(object):
+    def __init__(self, filename, mode='rt'):
+        self.filename = filename
+        self.mode = mode
+
+    def __enter__(self):
+        if self.get_file_format(self.filename) in ["csv", 'plain-text']:
+            self.handle = open(self.filename, self.mode)
+        elif self.get_file_format(self.filename) == "gzip":
+            self.handle = gzip.open(self.filename, self.mode)
+        return self.handle
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.handle.close()
+
+    def get_file_format(self, filepath):
+        if filepath.endswith('.tmp'):
+            filepath = filepath[:-4]
+
+        _, ext = os.path.splitext(filepath)
+
+        if ext == ".gz":
+            return "gzip"
+        else:
+            return "plain-text"
+
+def makedirs(directory, isfile=False):
+    if isfile:
+        directory = os.path.dirname(directory)
+        if not directory:
+            return
+
+    try:
+        os.makedirs(directory)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
+
+def build_shell_script(command, tag, tempdir):
+    outfile = os.path.join(tempdir, "{}.sh".format(tag))
+    with open(outfile, 'w') as scriptfile:
+        scriptfile.write("#!/bin/bash\n")
+        if isinstance(command, list) or isinstance(command, tuple):
+            command = ' '.join(command) + '\n'
+        scriptfile.write(command)
+    return outfile
+
+
+def run_cmd(cmd, output=None):
+    stdout = PIPE
+    if output:
+        stdout = open(output, "w")
+
+    cmd = ' '.join(cmd)
+    print(cmd)
+
+    p = Popen(cmd, stdout=stdout, stderr=PIPE, shell=True)
+
+    cmdout, cmderr = p.communicate()
+    retc = p.returncode
+
+    if retc:
+        raise Exception(
+            "command failed. stderr:{}, stdout:{}".format(
+                cmdout,
+                cmderr))
+
+    if output:
+        stdout.close()
+
+
+def run_in_gnu_parallel(commands, tempdir, ncores=None):
+    makedirs(tempdir)
+
+    scriptfiles = []
+
+    for tag, command in enumerate(commands):
+        scriptfiles.append(build_shell_script(command, tag, tempdir))
+
+    parallel_outfile = os.path.join(tempdir, "commands")
+    with open(parallel_outfile, 'w') as outfile:
+        for scriptfile in scriptfiles:
+            outfile.write("sh {}\n".format(scriptfile))
+
+    if not ncores:
+        ncores = str(multiprocessing.cpu_count())
+
+    gnu_parallel_cmd = ['parallel', '--jobs', ncores, '<', parallel_outfile]
+
+    run_cmd(gnu_parallel_cmd)
+
+
