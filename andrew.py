9eda9eaa4e647135d5876dc2564be65dd02852ac
Spenca <vatrtwaa@mskcc.org>
2020-06-23 15:44:07 -0400

    Testing

--- andrew.py
+++ andrew.py
@@ -218,9 +218,10 @@ def main(input_yaml, path_to_output_study, temp_dir):
         gistic_data['is_hdel'] = gistic_data['is_hdel'].fillna(0).astype(int)
         gistic_data.loc[gistic_data['is_hdel'] == 1, 'gistic_value'] = -2
 
-        gistic_data = hgnc_lookup(gistic_data, hgnc_file)
+        # gistic_data = hgnc_lookup(gistic_data, hgnc_file)
         # gistic_matrix = gistic_data.set_index(['Hugo_Symbol', 'Entrez_Gene_Id', 'sample'])['gistic_value'].unstack()
-        gistic_matrix = gistic_data.set_index(['Hugo_Symbol', 'Entrez_Gene_Id', 'sample'])['gistic_value']        
+        gistic_data = gistic_data.rename(columns={'gene_name': 'Hugo_Symbol'})
+        gistic_matrix = gistic_data.set_index(['Hugo_Symbol', 'sample'])['gistic_value']        
         
         print(gistic_matrix)
 
